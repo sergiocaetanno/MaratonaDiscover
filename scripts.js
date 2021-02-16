b038c4d89dcff74a7f58958e73c456265edb4cd1
Sérgio C. Júnior <sergiocaetanojunior15@gmail.com>
2021-02-16 15:37:53 -0300

    Atualiza código com funcionalidades do app em sua versão final

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let Modal = {
-    open(){
+    open() {
         //Abrir modal
         //Adicionar a class active ao modal
         document.querySelector('.modal-overlay').classList.add('active')
@@ -14,56 +14,136 @@ let Modal = {
 const transactions = [
     {
         id: 1,
-        description:'Luz',
+        description: 'Luz',
         amount: -50000,
         date: '23/01/2021'
     },
 
     {
         id: 2,
-        description:'Website',
+        description: 'Website',
         amount: 500000,
         date: '23/01/2021'
     },
 
     {
         id: 3,
-        description:'Internet',
+        description: 'Internet',
         amount: -20000,
         date: '23/01/2021'
     }
 ]
 
 const Transaction = {
-    incomes() {
-        // somar as entradas
-    },
-    expenses(){
+    all: transactions,
 
+    add(transaction){
+        Transaction.all.push(transaction)
+        App.reload()
     },
-    total(){
-        return this.incomes()-this.expenses()
+
+    incomes() {
+        //sum all income transactions
+        let income = 0
+        Transaction.all.forEach(function (transaction) {
+            if (transaction.amount >= 0) {
+                income += transaction.amount
+            }
+        })
+        return income
+    },
+    expenses() {
+        let expense = 0
+        Transaction.all.forEach(function (transaction) {
+            if (transaction.amount < 0) {
+                expense += transaction.amount
+            }
+        })
+        return expense
+    },
+    total() {
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
 const DOM = {
-    addTransaction(transactions, index){
-        console.log('cheguei')
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction()
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        this.transactionsContainer.appendChild(tr)
     },
-    innerHTMLTransaction(){
+    innerHTMLTransaction(transaction) {
+
+        const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+
+        const amount = Utils.formatCurrency(transaction.amount)
 
         const html = `
-                    <tr>
-                        <td class="description">Luz</td>
-                        <td class="expense">- R$ 500,00</td>
-                        <td class="date">23/01/2021</td>
-                        <td><img src="./assets/minus.svg" alt="Remover transação"></td>
-                    </tr>
+                    
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">- R$ ${transaction.date}</td>
+            <td class="date">${amount}</td>
+            <td><img src="./assets/minus.svg" alt="Remover transação"></td>
+
         `
         return html
+    },
+    updateBalance() {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes())
+
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.expenses())
+
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+    clearTransactions(){
+        DOM.transactionsContainer.innerHTML = ""
     }
 }
 
-DOM.addTransaction()
+const Utils = {
+    formatCurrency(value) {
+        let signal = Number(value) < 0 ? '-' : ''
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-Br", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+
+    }
+}
+
+const App = {
+    init(){
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+
+        DOM.updateBalance()
+    },
+    reload(){
+
+        DOM.clearTransactions()
+        App.init()
+
+    }
+}
+
+App.init()
+
+
+
+
+
